dbbd10dfaa2a423cee5890aadd09705388c7f1e1
Caroline Vieira <carolinesanvieira@gmail.com>
2021-10-26 17:49:27 +0200

    Agents Sketch

--- my-sketches/sketch-02.js
+++ my-sketches/sketch-02.js
@@ -4,8 +4,8 @@ const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
-
 /*const degToRad = (degrees) => {
   return (degrees / 180) * Math.PI;
 };
@@ -28,8 +28,8 @@ const sketch = () => {
 
     let x, y;
 
-    const num = 15;
-    const radius = width * 0.3;
+    const num = 10;
+    const radius = width * 0.4;
 
     for (let i = 0; i < num; i++) {
       const slice = math.degToRad(360 / num);
@@ -41,10 +41,10 @@ const sketch = () => {
       context.save();
       context.translate(x, y);
       context.rotate(-angle);
-      context.scale(random.range(3, 1), 1), random.range(0.3, 0.1);
+      context.scale(random.range(3, 1), 1), random.range(0.4, 0.5);
 
       context.beginPath();
-      context.rect(w * 0, 5, random.range(0, -h * 0.5), w, h);
+      context.rect(0 * w * 0.6, random.range(0, -h * 0.7), w, h);
       context.fill();
       context.restore();
 
@@ -52,15 +52,15 @@ const sketch = () => {
       context.translate(cx, cy);
       context.rotate(-angle);
 
-      context.lineWidth = random.range(3, 22);
+      context.lineWidth = random.range(2, 7);
 
       context.beginPath();
       context.arc(
         0,
         0,
-        radius * random.range(0.5, 1.4),
-        slice * 0.3,
-        slice * -0.8
+        radius * random.range(0.2, 1.5),
+        slice * random.range(0.2, 0, 9),
+        slice * random.range(-2, -8)
       );
       context.stroke();
 
