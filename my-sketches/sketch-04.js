ec6fecee3a3e12b0f55e82b50a937ce96a1d9999
Caroline Vieira <carolinesanvieira@gmail.com>
2021-10-28 01:05:57 +0200

    Sketch

--- my-sketches/sketch-04.js
+++ my-sketches/sketch-04.js
@@ -1,18 +1,31 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
 const math = require("canvas-sketch-util/math");
+const Tweakpane = require("tweakpane");
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
 };
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 0.2,
+  frame: 0,
+  animate: true,
+  lineCap: "butt",
+};
 
 const sketch = () => {
   return ({ context, width, height, frame }) => {
     // Create gradient
-    var grd = context.createLinearGradient(255, 150, 2, 100, 160, 200);
-    grd.addColorStop(0, "#08203E");
-    grd.addColorStop(1, "#223F5A");
+    var grd = context.createLinearGradient(255, 0, 130, 100, 16, 0);
+    grd.addColorStop(1, "#A9FF68");
+    grd.addColorStop(0.7, "#638B95");
+    grd.addColorStop(0, "#34073D");
 
     // Fill with gradient
     context.fillStyle = grd;
@@ -20,8 +33,8 @@ const sketch = () => {
 
     context.fillRect(0, 0, width, height);
 
-    const cols = 9;
-    const rows = 9;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -40,9 +53,12 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
-      const n = random.noise2D(x + frame * 10, y, 0.0011);
-      const angle = n * Math.PI * 0.2;
-      const scale = math.mapRange(n, -1, 0.2, 0.1, 40);
+      const f = params.animate ? frame : params.frame;
+
+      //const n = random.noise2D(x + frame * 10, y, 0.0011);
+      const n = random.noise3D(x, y, f * 10, params.freq);
+      const angle = n * Math.PI * params.amp;
+      const scale = math.mapRange(n, -1, 0.2, params.scaleMin, params.scaleMax);
 
       context.save();
       context.translate(x, y);
@@ -51,6 +67,7 @@ const sketch = () => {
       context.rotate(angle);
 
       context.lineWidth = scale;
+      context.lineCap = params.lineCap;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
@@ -61,5 +78,26 @@ const sketch = () => {
     }
   };
 };
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+
+  folder = pane.addFolder({ title: "Grid " });
+  folder.addInput(params, "lineCap", {
+    options: { butt: "butt", round: "round", square: "square" },
+  });
+  folder.addInput(params, "cols", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "rows", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "scaleMin", { min: 1, max: 100 });
+  folder.addInput(params, "scaleMax", { min: 1, max: 100 });
+
+  folder = pane.addFolder({ title: "Noise" });
+  folder.addInput(params, "freq", { min: -0.01, max: 0.01 });
+  folder.addInput(params, "amp", { min: 0, max: 1 });
+  folder.addInput(params, "animate");
+  folder.addInput(params, "frame", { min: 0, max: 999 });
+};
+
+createPane();
 
 canvasSketch(sketch, settings);
